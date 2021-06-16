ce7cb68d2c3abe1ad67dd29286000789d7ab1f81
dawangk <85403980+dawangk@users.noreply.github.com>
2021-06-15 21:46:49 -0400

    Create Two Sets II.cpp

---
+++ Knapsack DP/Two Sets II.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define inputJunk ios_base::sync_with_stdio(0); cin.tie(0);
+#define pb push_back
+#define f first
+#define s second
+#define debug cout<<"HERE"<<endl;
+#define ell "\n"
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+ 
+ 
+const int INF = 1e9+250;
+const int MM = 5e3+5; int mod = 2014;
+ 
+ 
+int main() {
+  int mod = 1e9+7;
+  int n;
+  cin >> n;
+  int target = n*(n+1)/2;
+  if (target%2) {
+    cout << 0 << endl;
+    return 0;
+  }
+  target /= 2;
+ 
+  vector<vector<int>> dp(n,vector<int>(target+1,0));
+  dp[0][0] = 1;
+  for (int i = 1; i < n; i++) {
+    for (int j = 0; j <= target; j++) {
+      dp[i][j] = dp[i-1][j];
+      int left = j-i;
+      if (left >= 0) {
+	(dp[i][j] += dp[i-1][left]) %= mod;
+      }
+    }
+  }
+  cout << dp[n-1][target] << endl;
+}
