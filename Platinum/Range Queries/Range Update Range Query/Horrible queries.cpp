1026315176807cbc9960ba71810441a95796fed8
dawangk <85403980+dawangk@users.noreply.github.com>
2021-08-23 10:55:00 -0400

    Create Horrible queries.cpp

---
+++ Platinum/Range Queries/Range Update Range Query/Horrible queries.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#include <ext/rope>
+//using namespace __gnu_cxx;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <stdlib.h>
+using namespace __gnu_pbds;
+
+//#pragma GCC optimize("Ofast,unroll-loops")
+//#pragma GCC target("avx,avx2,fma")
+#define inputJunk ios_base::sync_with_stdio(0); cin.tie(0);
+#define pb push_back
+#define f first
+#define s second
+#define all(x) x.begin(), x.end()
+#define debug cout<<"HERE"<<endl;
+#define ell "\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pll;
+typedef pair<int, pi> trip;
+typedef pair<pll, ll> tripll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<pll> vpll;
+typedef vector<trip> vtrip;
+typedef vector<tripll> vtripll;
+
+const int INF = 1e9+1212;
+const ll P = 9973, M = 1e9+9;
+const int MM = 1e5+5; int mod = 998244353;//1e9+7
+
+template <class T, int ...Ns> struct BIT {
+	T val = 0; void upd(T v) { val += v; }
+	T query() { return val; }
+};
+template <class T, int N, int... Ns> struct BIT<T, N, Ns...> {
+	BIT<T,Ns...> bit[N+1];
+	template<typename... Args> void upd(int pos, Args... args) { assert(pos > 0);
+		for (; pos<=N; pos+=pos&-pos) bit[pos].upd(args...); }
+	template<typename... Args> T sum(int r, Args... args) {
+		T res=0; for (;r;r-=r&-r) res += bit[r].query(args...);
+		return res; }
+	template<typename... Args> T query(int l, int r, Args...
+		args) { return sum(r,args...)-sum(l-1,args...); }
+};
+
+template<class T, int SZ> struct BITrange {
+	BIT<T,SZ> bit[2];
+	void upd(int hi, T val) {
+		bit[1].upd(1,val), bit[1].upd(hi+1,-val);
+		bit[0].upd(hi+1,hi*val);
+	}
+	void upd(int lo,int hi,T val){upd(lo-1,-val),upd(hi,val);}
+	T sum(int x) { return bit[1].sum(x)*x+bit[0].sum(x); }
+	T query(int x, int y) { return sum(y)-sum(x-1); }
+};
+
+int main(){
+    inputJunk
+    int t; cin>>t;
+    while(t--){
+        int n, q; cin>>n>>q;
+        BITrange<ll, MM> bit;
+        for(int i= 0;i<q;i++){
+            int a; cin>>a;
+            if(a){
+                ll b, c; cin>>b>>c;
+                cout<<bit.query(b, c)<<ell;
+            }else{
+                ll b, c, d; cin>>b>>c>>d;
+                bit.upd(b, c, d);
+            }
+        }
+    }
+
+}
+/*CUSTOM TEST CASE 1
+*/
+
+/*CUSTOM TEST CASE 2
+*/
+
+/*CUSTOM TEST CASE 3
+*/
+
+/*Comments of Shame
+- floating error (use integer division instead)
+- cin vs getline
+- upperbound and lowerbound returns iteratorsf
+- use long long when number is big enough
+- for dp invalid cases needs to be skipped
+- some base cases won't work (review cow poetry)
+- always check bounds, some TLE are due to incorrect bounds!
+- dont mess up return types
+= RESET ARRAYS!!!!!!!!!!
+- USE UR BRAIN
+- INF setting problems
+- push vs pb
+- check if things are used properly
+- read the PROBLEM (directed vs undirected graph)
+*/
+/*
+    freopen("time.in","r", stdin);
+    freopen("time.out","w", stdout);
+*/
